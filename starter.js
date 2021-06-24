2c44a7b8db6b2585fe821df1fd2309a7326346b2
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-06-24 08:56:47 -0700

    done with errors

--- starter.js
+++ starter.js
@@ -1,22 +1,47 @@
-// 1.
-function sum(array) {
-  let sum = 0;
+// // 1.
+// function sum(array) {
 
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum;
-}
+//   let sum = 0;
 
-function handleError(error) {}
+//   for (let i = 0; i < array.length; i++) {
+//     sum += array[i];
+//   }
+//   return sum;
+// }
 
-let res = sum(null);
-console.log(res);
+// function handleError(error) {
+//   console.log("HANDLE ERROR: ",error)
+// }
+
+
+// try{
+//   let res = sum(null);
+//   console.log(res);
+// } catch (error) {
+//   handleError(error)
+// }
+
 
 // 2.
-function sayName(name) {}
+function sayName(name) {
+  // console.log("first",!name instanceof String)
+  // console.log(!(name instanceof String))
+  if(typeof name !== "string") throw Error("Not a string")
+  console.log(name)
+}
 
 try {
-} catch (error) {}
+  sayName([])
 
-// 3.
+} catch (error) {
+  console.log("Caught")
+  console.log(error)
+}
+
+class ValidationError extends Error {
+  constructor(){
+
+  }
+}
+
+throw new ValidationError("This is our class")
